54a3ce976a046725768a64d4124391f8e022c5a3
Alex Gontcharov <lavebug@hotmail.com>
2018-07-23 14:46:06 -0600

    converted json/string to dictionary

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+let medicationMap = {};
+let prescriptionMap = {};
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -29,6 +32,7 @@ http.get('http://api-sandbox.pillpack.com/prescriptions', (resp) => {
   // the whole response has been received.
   resp.on('end', () => {
     prescriptions = JSON.parse(data);
+    prescriptions.forEach(prescription => {prescriptionMap[prescription.id] = prescription;});
   });
  
 }).on("error", (err) => {
@@ -48,38 +52,35 @@ http.get('http://api-sandbox.pillpack.com/medications', (resp) => {
   // the whole response has been received.
   resp.on('end', () => {
     medications = JSON.parse(data);
+    medications.forEach(medication => {medicationMap[medication.id] = medication;});
   });
  
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
 
-// TODO improve/combine for loop (map, reduce, filter) and get rid of setTimeout
+// TODO get rid of setTimeout
 setTimeout(function(){
-  let arrayGenRccui = [];
-  // Itearate prescriptions and medications on every medication id match modify prescriptions by adding rxcui and generic key/value
-  for (let i = 0 ; i < prescriptions.length ; i++){
-    for (let j = 0; j < medications.length ; j++){
-      if (prescriptions[i].medication_id == medications[j].id){
-        prescriptions[i].rxcui = medications[j].rxcui;
-        prescriptions[i].generic = medications[j].generic;
-      }
-    };  
-  };
-  // TODO improve/combine for loop (map, reduce, filter)
-  // Itearate modified prescriptions and medications
-  for (let i = 0 ; i < prescriptions.length ; i++){
-    for (let j = 0; j < medications.length ; j++){
-      if (prescriptions[i].generic === false && prescriptions[i].rxcui == medications[j].rxcui){
-        prescriptions[i].generic_med_id = medications[j].id;
-        arrayGenRccui.push({"prescription_id": prescriptions[i].id, "medication_id": prescriptions[i].generic_med_id})
+  let updatedPrescriptions = [];
+  for(let x = 0; x < prescriptions.length; x++) {
+    let medicationObj = medicationMap[prescriptionMap[prescriptions[x].id].medication_id];
+    let arrayMedicationResult = [];
+    let j = 0;
+    for (let i = 0; i < medications.length; i++){
+      if (medicationObj.rxcui == medications[i].rxcui){
+        arrayMedicationResult[j] = medications[i];
+        j++;
       }
     };
-  };
-  // Log out prescriptions update as per gist example https://gist.github.com/mpokress/51e7956d082656d52fbb
-  // Just a prescription_id and (generic) mdeication_id
-  let prescription_updates = {"prescription_updates": arrayGenRccui}
-  console.log(prescription_updates);
+    for (let i = 0; i < arrayMedicationResult.length ; i++){
+      if(arrayMedicationResult[i].generic === true) {
+        updatedPrescriptions.push({"prescription_id": prescriptionMap[prescriptions[x].id].id, "medication_id": arrayMedicationResult[i].id});
+      }
+    };
+  }
+  
+  let prescription_updates = {"prescription_updates": updatedPrescriptions}
+  // console.log(prescription_updates);
 }, 1000);
 
 // TODO Create a view (index.html) and output that to the page
