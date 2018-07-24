98b2c2293e1169840dc59562f31f924a151e01c6
Alex Gontcharov <lavebug@hotmail.com>
2018-07-24 09:52:16 -0600

    cleand up comments

--- app.js
+++ app.js
@@ -17,29 +17,29 @@ function getMedications() {
 axios.all([getPrescriptions(), getMedications()])
   .then(axios.spread(function (pres, meds) {
     // Both requests are now complete
-    // convert it to dictionary key/value pair -> n
-    prescriptions = pres.data;
+    prescriptions = pres.data; // retriving prescriptions colection
+    // convert to dictionary key/value pair where key is prescription id and value is a prescription object
     prescriptions.forEach(prescription => {prescriptionMap[prescription.id] = prescription;});
-    medications = meds.data;
+    medications = meds.data; // retriving medications collection
+    // convert to dictionary key/value pair where key is medication id and value is a medication object
     medications.forEach(medication => {medicationMap[medication.id] = medication;});
 
-    
-    // for loop of prescriptions retrieving medication from dictionary -> n+1
-    // total time complexity (wort case) = O(n+n+1)
-    //                                   = O(2n+1)
-    //                                   = O(n)
+    // for loop of prescriptions retrieving medication from dictionary
     let updatedPrescriptions = [];
     for(let i = 0; i < prescriptions.length; i++) {
+      // retriving medication object based on medication_id from prescription
       let medicationObj = medicationMap[prescriptionMap[prescriptions[i].id].medication_id];
       let arrayMedicationResult = [];
       let j = 0;
       for (let o = 0; o < medications.length; o++){
+        // finding a medication with same rxcui in medication colection
         if (medicationObj.rxcui == medications[o].rxcui){
           arrayMedicationResult[j] = medications[o];
           j++;
         }
       };
       for (let u = 0; u < arrayMedicationResult.length ; u++){
+        // filtering brand medication storing result in updatedPrescriptions
         if(arrayMedicationResult[u].generic === true) {
           updatedPrescriptions.push({"prescription_id": prescriptionMap[prescriptions[u].id].id, "medication_id": arrayMedicationResult[u].id});
         }
