9d8a11c3c2fa2d99c253786dd3c8c68fffdccd39
Alex Gontcharov <lavebug@hotmail.com>
2018-07-23 18:15:37 -0600

    added axio for multiple request with async/await, removed http & settimeout

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
 const http = require('http');
+const axios = require('axios');
+let medicationMap = {};
+let prescriptionMap = {};
+let prescriptions = [];
+let medications = [];
+
+// Use axios for multiple request 
+function getPrescriptions() {
+  return axios.get('http://api-sandbox.pillpack.com/prescriptions');
+}
+
+function getMedications() {
+  return axios.get('http://api-sandbox.pillpack.com/medications');
+}
+
+axios.all([getPrescriptions(), getMedications()])
+  .then(axios.spread(function (pres, meds) {
+    // Both requests are now complete
+    // convert it to dictionary key/value pair -> n
+    prescriptions = pres.data;
+    prescriptions.forEach(prescription => {prescriptionMap[prescription.id] = prescription;});
+    medications = meds.data;
+    medications.forEach(medication => {medicationMap[medication.id] = medication;});
+
+    // for loop of prescriptions retrieving medication from dictionary -> n+1
+    // total time complexity (wort case) = O(n+n+1)
+    //                                   = O(2n+1)
+    //                                   = O(n)
+    let updatedPrescriptions = [];
+    for(let x = 0; x < prescriptions.length; x++) {
+      let medicationObj = medicationMap[prescriptionMap[prescriptions[x].id].medication_id];
+      let arrayMedicationResult = [];
+      let j = 0;
+      for (let i = 0; i < medications.length; i++){
+        if (medicationObj.rxcui == medications[i].rxcui){
+          arrayMedicationResult[j] = medications[i];
+          j++;
+        }
+      };
+      for (let i = 0; i < arrayMedicationResult.length ; i++){
+        if(arrayMedicationResult[i].generic === true) {
+          updatedPrescriptions.push({"prescription_id": prescriptionMap[prescriptions[x].id].id, "medication_id": arrayMedicationResult[i].id});
+        }
+      };
+    }
+
+    let prescription_updates = {"prescription_updates": updatedPrescriptions}
+    console.log(prescription_updates);
+  }));
 
 // Create/start Node
 const hostname = '127.0.0.1';
 const port = 3000;
-
-let medicationMap = {};
-let prescriptionMap = {};
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -17,70 +63,4 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// TODO Combine prescritions and medications in one get (foreach)
-// Get Prescriptions (500)
-let prescriptions = [];
-let medications = [];
-http.get('http://api-sandbox.pillpack.com/prescriptions', (resp) => {
-  let data = '';
- 
-  // data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
- 
-  // the whole response has been received.
-  resp.on('end', () => {
-    prescriptions = JSON.parse(data);
-    prescriptions.forEach(prescription => {prescriptionMap[prescription.id] = prescription;});
-  });
- 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-// TODO Combine prescritions and medications in one get (foreach)
-// Get Medications (40)
-http.get('http://api-sandbox.pillpack.com/medications', (resp) => {
-  let data = '';
- 
-  // data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
- 
-  // the whole response has been received.
-  resp.on('end', () => {
-    medications = JSON.parse(data);
-    medications.forEach(medication => {medicationMap[medication.id] = medication;});
-  });
- 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-// TODO get rid of setTimeout
-setTimeout(function(){
-  let updatedPrescriptions = [];
-  for(let x = 0; x < prescriptions.length; x++) {
-    let medicationObj = medicationMap[prescriptionMap[prescriptions[x].id].medication_id];
-    let arrayMedicationResult = [];
-    let j = 0;
-    for (let i = 0; i < medications.length; i++){
-      if (medicationObj.rxcui == medications[i].rxcui){
-        arrayMedicationResult[j] = medications[i];
-        j++;
-      }
-    };
-    for (let i = 0; i < arrayMedicationResult.length ; i++){
-      if(arrayMedicationResult[i].generic === true) {
-        updatedPrescriptions.push({"prescription_id": prescriptionMap[prescriptions[x].id].id, "medication_id": arrayMedicationResult[i].id});
-      }
-    };
-  }
-  
-  let prescription_updates = {"prescription_updates": updatedPrescriptions}
-  // console.log(prescription_updates);
-}, 1000);
-
-// TODO Create a view (index.html) and output that to the page
+// TODO Create a view (index.html) & output that to the page
